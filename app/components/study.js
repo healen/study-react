ec7bb83e1fb7ea91164959f6479442c709bee239
张晓东 <zhangxiaodong@zhangxiaodongdeMacBook-Air.local>
2017-03-21 17:05:59 +0800

    瞎写的

--- app/components/study.js
+++ app/components/study.js
@@ -13,44 +13,37 @@ class Hello extends Component {
 	render(){
 		return (
 			<div>
-				{
-					// <Example title="React Hello World 实例" codeQure={(
-					// 	<img src={require("./images/b1.png")} width="100%"/>
-					// )}>
-				}
 				<Example title="React Hello World 实例" codeQure={(<img src={require("./images/b1.png")} width="100%"/>)}>
 					<HelloWrold />
 				</Example>
-
 				<Example title="React Hello World 升级版双向数据绑定 实例" codeQure={(<img src={require("./images/b2.png")} width="100%"/>)}>
 					<TowWayData />
-
 				</Example>
-
 			</div>
-			
 		)
 	}
 }
 
 class JsxSyntx extends Component {
 	render(){
-		var Arr = ['held','bood','sopd']
 		return (
-			<Example title="React JSX语法" codeQure={(<img src={require("./images/b3.png")} width="100%"/>)}>
-				<ul>
-					{
-						Arr.map(function(name,key){
-							return (
-								<li key={key}>hello , {name}</li>
-							)
-						})
-					}
-				</ul>
-			</Example>
+			<div>
+				<Example title="React JSX语法" codeQure={(<img src={require("./images/b3.png")} width="100%"/>)}>
+					<Arrjsx array={['abc','efg','kim']} />
+				</Example>
+				<Example title="React JSX语法" codeQure={(
+						<img src={require("./images/b4.png")} width="100%"/>
+					
+					)}>
+					<Arrjsx2 />
+				</Example>
+			</div>
 		)
 	}
 }
+
+
+
 
 
 
@@ -64,15 +57,12 @@ class HelloWrold extends Component {
 		return (
 			<div>hello world</div>
 		)
-		
 	}
 }
-
-
+/*双向数据绑定*/
 class TowWayData extends Component {
 	constructor(props) {
 	  super(props);
-	
 	  this.state = {
 	  	name:""
 	  };
@@ -89,18 +79,31 @@ class TowWayData extends Component {
 		this.setState({name:e.target.value})
 	}
 }
+/*JSX 操作*/
+class Arrjsx extends Component {
+	render(){
+		return (
+			<ul>
+				{
+					this.props.array.map(function(name,key){
+						return (
+							<li key={key}>hello , {name}</li>
+						)
+					})
+				}
+			</ul>
+		)
 
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+class Arrjsx2 extends Component {
+	render(){
+		/*<div dangerouslySetInnerHTML={{__html:this.props.array}}></div>*/
+		var arr = [<h2>Hello World</h2>,<h5>this is a Array oop</h5>];
+		return (
+			<div>{arr}</div>
+		)
+	}
+}
 export {Hello,JsxSyntx};
